a3689ef3a3ebb59a8072cf5ed7f93567b1d211f4
Adriano Júnior <107235038+Adriano-AJ@users.noreply.github.com>
2026-08-28 07:13:36 -0300

    Add files via upload

---
+++ CBTSWE2_TP01/CBTSWE2_TP01/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Integrantes:
+ * 
+ * Luiz Gustavo Verissimo Monteiro - CB3030326
+ * 
+ */
+
+using CBTSWE2_TP01;
+using CBTSWE2_TP01.Repositorio;
+using CBTSWE2_TP01.Testes;
+using Microsoft.AspNetCore.Hosting;
+
+// Executa a sua classe de testes
+ BooktTest.Executar();
+
+
+ Console.ReadLine();
+ 
+
+var _repo = new BookRepositoryCSV();
+
+IWebHost host = new WebHostBuilder()
+    .UseKestrel()
+    .UseStartup<Startup>()
+    .Build();
+
+host.Run();
