0121554725e3c95fa9493d2b474d5506413c6367
Adriano Júnior <107235038+Adriano-AJ@users.noreply.github.com>
2026-08-28 07:20:14 -0300

    Refactor BookRepositoryCSV for clarity and structure

--- CBTSWE2_TP01/CBTSWE2_TP01/Repositorio/BookRepositoryCSV.cs
+++ CBTSWE2_TP01/CBTSWE2_TP01/Repositorio/BookRepositoryCSV.cs
@@ -1,10 +1,4 @@
-﻿/*
- * Integrantes:
- * 
- * Luiz Gustavo Verissimo Monteiro - CB3030326
- * 
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
